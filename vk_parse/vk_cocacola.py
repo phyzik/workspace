bb8d934a639a56dce8a946a13ca3669355f7d205
Sheverev <gmail@gmail.com>
2013-06-09 12:54:40 -0700

    working version

--- vk_parse/vk_cocacola.py
+++ vk_parse/vk_cocacola.py
@@ -72,6 +72,26 @@ def gethtml( n, topic, getvar='offset' ):
 items = {}
 dates = {}
 today = datetime.datetime.today()
+def setMoscowTZ(): os.environ['TZ'] = 'Europe/Moscow'
+def setPSTTZ(): os.environ['TZ'] = 'America/Los_Angeles'
+class MoscowTime():
+    def __init__(self):
+        setMoscowTZ()
+        self.ts = datetime.datetime.today()
+        self.ys = self.ts - datetime.timedelta(1)
+        self.date = self.ts.strftime( "%Y/%m/%d" )
+        self.time = self.ts.strftime( "%H:%M" )
+        self.timedb = self.ts.strftime( "%H:%M:%S" )
+        self.today = self.ts.strftime( "%d.%m" )
+        self.yesterday = self.ys.strftime( "%d.%m" )
+        self.y = self.ts.strftime( "%Y" )
+        self.yy = self.ys.strftime( "%Y" )
+        self.m = self.ts.strftime( "%m" )
+        self.ym = self.ys.strftime( "%m" )
+        self.d = self.ts.strftime( "%d" )
+        self.yd = self.ys.strftime( "%d" )
+        setPSTTZ()
+
 
 class ComparableMixin(object):
     def _compare(self, other, method):
@@ -91,40 +111,42 @@ class ComparableMixin(object):
     
 class RuDate(ComparableMixin):
     def __init__(self, s):
+        self.ms = MoscowTime()
         (self.d, self.m, self.y) = self.dat_parse(s)
-        self.dat = "%02d.%02d.%04d" % (self.d, self.m, self.y)
+        self.date = "%s.%s.%s" % (self.d, self.m, self.y)
+        self.dbdate = "%s-%s-%s" % (self.y, self.m, self.d )
         
     def dat_parse(self, s):
         dd = s.split(' ')
         if len(dd) > 1:
-            if dd[1] == u'янв': m = 1
-            elif dd[1] == u'фев': m = 2
-            elif dd[1] == u'мар': m = 3
-            elif dd[1] == u'апр': m = 4
-            elif dd[1] == u'мая': m = 5
-            elif dd[1] == u'июн': m = 6
-            elif dd[1] == u'июл': m = 7
-            elif dd[1] == u'авг': m = 8
-            elif dd[1] == u'сен': m = 9
-            elif dd[1] == u'окт': m = 10
-            elif dd[1] == u'ноя': m = 11
-            elif dd[1] == u'дек': m = 12
-            else: m = 13
+            if dd[1] == u'янв': m = '01'
+            elif dd[1] == u'фев': m = '02'
+            elif dd[1] == u'мар': m = '03'
+            elif dd[1] == u'апр': m = '04'
+            elif dd[1] == u'мая': m = '05'
+            elif dd[1] == u'июн': m = '06'
+            elif dd[1] == u'июл': m = '07'
+            elif dd[1] == u'авг': m = '08'
+            elif dd[1] == u'сен': m = '09'
+            elif dd[1] == u'окт': m = '10'
+            elif dd[1] == u'ноя': m = '11'
+            elif dd[1] == u'дек': m = '12'
+            else: m = '01'
             if len( dd) > 2:
-                return (int(dd[0]), m, int(dd[2]))
+                return ("%02d" % int(dd[0]), m, dd[2])
             else:
-                return (int(dd[0]), m, 0)
+                return ("%02d" % int(dd[0]), m, self.ms.y)
         elif s == u'вчера':
-            return (32,12,2222)
+            return (self.ms.yd, self.ms.ym, self.ms.yy)
         elif s == u'сегодня':
-            return (33,12,2222)
+            return (self.ms.d, self.ms.m, self.ms.y)
         else:  
-            return (0,0,0)  
+            return ('01','01','1980')  
                
     def get(self):
         return self.dat
     def _cmpkey(self):
-        return self.y*10000 + self.m*100 + self.d
+        return int(self.y)*10000 + int(self.m)*100 + int(self.d)
     def __repr__(self):
         return self.dat
 
@@ -141,12 +163,14 @@ def items_list_wall(n, topic, topic_text, getvar, last=0, d=50):
     a_all = detr.findAll( 'a')
     '''
     pi_author = detr.findAll( 'a', {'class':'pi_author'} )
-    pi_text = detr.findAll( 'div', {'class':'pi_text'} )
+    #pi_text = detr.findAll( 'div', {'class':'pi_text'} )
+    pi_body = detr.findAll( 'div', {'class':'pi_body'} )
     item_date = detr.findAll( 'a', {'class':'item_date'} )
     post_item = detr.findAll( 'div', {'class':'post_item comment_item'} )
     a_all = detr.findAll( 'a')
     a_anchors = []
-    i,ni,nf,nn = 0,0,0,0
+    i,ni,nf = 0,0,0
+    nn = last+1
     dat = None
     stop = None
     count = 0
@@ -163,21 +187,23 @@ def items_list_wall(n, topic, topic_text, getvar, last=0, d=50):
             continue 
         msgURL, dat = item_date[i].attrMap['href'],  item_date[i].text
         aURL, author = pi_author[i+1].attrMap['href'], pi_author[i+1].text
+        
         dt = dat.split(u' в ')
         dd = RuDate(dt[0])
         tt = dt[1]
-        try:
-            msg = pi_text[i+1].text
-        except:
-            msg = ''
-            print '@@@ %d,%d @@@'%(i+1,ni),
+        msg = pi_body[i+1].findChild('div', {'class':'pi_text',})
+        if None == msg: msg = '<picture>'
+        else: msg = msg.text  
+        likes = pi_body[i+1].findChild('a', {'class':'item_like _i'})
+        if likes == None: likes = 0
+        else: likes = likes.text
         u"Дата    Тема    URL  author    URLauth    Текст    URLcomm"
         if nf == 0 : nf = ni
         nn = ni
 
-        print 'dat:', dat, 'nn,last:', nn,last
-        if dd >= RuDate(u'3 июн') and nn > last:  
-            items[nn] = ( dat, topic_text, topic, author, aURL, msg, msgURL )
+        #print 'dat:', dat, 'nn,last:', nn,last
+        if dd >= RuDate(u'4 июн') and nn > last:  
+            items[nn] = ( dat, topic_text, topic, author, aURL, msg, msgURL, likes )
             topics[topic]['lastN'] = nn
             topics[topic]['count'] += 1
             count += 1
@@ -185,7 +211,7 @@ def items_list_wall(n, topic, topic_text, getvar, last=0, d=50):
             dates[nn] = {'date':dd.get(), 'time':tt}
             if topics[topic]['firstN'] == 0: topics[topic]['firstN'] = nn
         else: 
-            print dat, 'break!!!!!!!!', topic
+            #print dat, 'break!!!!!!!!', topic
             stop = True
 
     print
@@ -213,7 +239,8 @@ def items_list(n, topic, topic_text, getvar, last=0, d=20):
     print len (item_date)
     a_all = detr.findAll( 'a')
     
-    i,ni,nf,nn,tt = 0,0,0,0,''
+    i,ni,nf,tt = 0,0,0,''
+    nn = last+1
     count = 0
     for a in a_all:
         if 'name' in a.attrMap.keys():
@@ -241,7 +268,7 @@ def items_list(n, topic, topic_text, getvar, last=0, d=20):
                     at = item.find('div', {'class':'pi_body'})
                 tt = ai.text
                 if nn > last:
-                    items[nn] = ( ai.text, topic_text, topic, aa.text, aa['href'], at.text, ai.attrMap['href'] )
+                    items[nn] = ( ai.text, topic_text, topic, aa.text, aa['href'], at.text, ai.attrMap['href'], 0 )
                     topics[topic]['lastN'] = nn
                     topics[topic]['count'] += 1
                     count += 1
@@ -252,14 +279,16 @@ def items_list(n, topic, topic_text, getvar, last=0, d=20):
 
                 i += 1
     
-    if count < d: 
-        stop = True
-    else: 
+    if count > 0 or nn <= last: 
         topics[topic]['offset'] += d
         stop = False
+        print 'stop=False; offset:%d, count=%d, nn=%d, last=%d, d=%d' % ( topics[topic]['offset'], count, nn, last,d )
+    else: 
+        stop = True
+        print 'stop=True; offset:%d, count=%d, nn=%d, last=%d, d=%d' % ( topics[topic]['offset'], count, nn, last, d )
     return (stop, count, nf,nn,tt)
 
-limit = 9999
+limit = 99999
 def fulllist(n, topic, topic_text, getvar, last=0):
     global limit, topics
     if 'wall' in topic: flist, d = items_list_wall, 50
@@ -290,7 +319,7 @@ def print_items(ofile, title=False):
         f_walls.write(u'')
         if title:
             titles = (u'#',u'Дата комментария',u'Тема дискуссии',u'URL дискуссии',
-                      u'Автор комментария',u'URL автора',u'Текст комментария',u'URL комментария',
+                      u'Автор комментария',u'URL автора',u'Текст комментария',u'URL комментария','Likes',
                       u'Тон (негатив, позитив, нейтральный)',u'Упоминание Coca-Cola',u'Привязка к теме поста (да/нет)',
                       u'Является ли вопросом (да/нет)',u'Тема комментария',u'Комментарий ПМ/КМ')
             f_topics.write((u'%s|' * len(titles)) % titles)
@@ -302,7 +331,7 @@ def print_items(ofile, title=False):
         #print
         #print k, "%s \n%s\n%s\n%s\n%s" % items[k]
         s = u"%d" % k
-        s += u'|%s|%s|http://vk.com/%s|%s|http://vk.com%s|%s|http://vk.com%s| \n' % items[k]
+        s += u'|%s|%s|http://vk.com/%s|%s|http://vk.com%s|%s|http://vk.com%s|%s| \n' % items[k]
         if 'topic' in items[k][2]:
             f_topics.write(s)
         elif 'wall' in items[k][2]:
@@ -327,23 +356,23 @@ def print_items(ofile, title=False):
 def itertopics(picklefile, newpic=True):
     global topics
     f = picklefile + '.pkl'
-    if newpic:
+    if newpic or True:
         topics['topic-16297716_28039770'] = {'offset':'offset', 'name':u'КОЛЛЕКЦИЯ СТАКАНОВ COCA-COLA «СОЧИ 2014», задаём вопросы в этой теме'}
         topics['topic-16297716_28124405'] = {'offset':'offset', 'name':u'КОЛЛЕКЦИЯ СТАКАНОВ COCA-COLA «СОЧИ 2014», обмен с другими участниками группы'}
         topics['topic-16297716_22922336'] = {'offset':'offset', 'name':u'ПРИЗЫ ОТ COCA-COLA, вопросы и обсуждения'}
         topics['topic-16297716_28039770']['firstN'] = 105737
-        topics['topic-16297716_28039770']['lastN'] = 107482#106434 #107482
-        topics['topic-16297716_28039770']['offset'] = 20 * (457 - 1)
+        topics['topic-16297716_28039770']['lastN'] = 109672#107482#106434 #107482
+        topics['topic-16297716_28039770']['offset'] = 20 * (587 - 1)
         topics['topic-16297716_28039770']['do'] = 1 
         topics['topic-16297716_28039770']['count'] = 0 
         topics['topic-16297716_28124405']['firstN'] = 105815
-        topics['topic-16297716_28124405']['lastN'] = 107442#106393 #107442
-        topics['topic-16297716_28124405']['offset'] = 20 * (12 - 1)
+        topics['topic-16297716_28124405']['lastN'] = 109673#107442#106393 #107442
+        topics['topic-16297716_28124405']['offset'] = 20 * (18 - 1)
         topics['topic-16297716_28124405']['do'] = 1
         topics['topic-16297716_28124405']['count'] = 0
         topics['topic-16297716_22922336']['firstN'] = 105762
-        topics['topic-16297716_22922336']['lastN'] = 107470#106435 #107470
-        topics['topic-16297716_22922336']['offset'] = 20 * (334 - 1)
+        topics['topic-16297716_22922336']['lastN'] = 109635#107470#106435 #107470
+        topics['topic-16297716_22922336']['offset'] = 20 * (347 - 1)
         topics['topic-16297716_22922336']['do'] = 1
         topics['topic-16297716_22922336']['count'] = 0
         topics['wall-16297716_192063'] = {'offset':'offset', 'name':u'Хочешь собрать коллекцию стаканов Coca-Cola «Сочи 2014»?'}
